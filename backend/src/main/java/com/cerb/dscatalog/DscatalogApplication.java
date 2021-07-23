d930f6150b07e75f5aba6a49a4dfd38dea407215
Jose Meurer <josewm18@gmail.com>
2021-07-23 20:37:47 -0300

    Backend project created

---
+++ backend/src/main/java/com/cerb/dscatalog/DscatalogApplication.java
@@ -0,0 +1,15 @@
+package com.cerb.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+		
+		System.out.println("Hello World!!!!");
+	}
+
+}
