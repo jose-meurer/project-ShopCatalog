9b4e31649a63ff82b262a6b12943174085870c4e
Jose Meurer <josewm18@gmail.com>
2021-07-30 18:02:35 -0300

    Class Category created

--- backend/src/main/java/com/cerb/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/cerb/dscatalog/DscatalogApplication.java
@@ -9,7 +9,6 @@ public class DscatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 		
-		System.out.println("Hello World!!!!");
 	}
 
 }
